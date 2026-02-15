8f8ebcd76abbcf8fa033d4aaff0e32d338efd45e
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-16 00:14:51 +0300

    Fix Room Query import for reminder observation DAO

--- app/src/main/java/com/leosoft/longevity/data/local/dao/QuickAddDao.kt
+++ app/src/main/java/com/leosoft/longevity/data/local/dao/QuickAddDao.kt
@@ -3,6 +3,7 @@ package com.leosoft.longevity.data.local.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.leosoft.longevity.data.local.entity.ReminderLogEntity
 import com.leosoft.longevity.data.local.entity.TaskLogEntity
 
