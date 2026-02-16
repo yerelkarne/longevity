f03ff2751823067e0b43289e3e227af659ed4ee3
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-16 14:34:52 +0300

    Fix reminder dialog height expansion around time field

--- app/src/main/java/com/leosoft/longevity/ui/screens/tabs/ModuleScreens.kt
+++ app/src/main/java/com/leosoft/longevity/ui/screens/tabs/ModuleScreens.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -402,7 +403,8 @@ private fun AddReminderDialog(
                         )
                         Box(
                             modifier = Modifier
-                                .fillMaxSize()
+                                .fillMaxWidth()
+                                .height(56.dp)
                                 .clickable { openTimePicker() }
                         )
                     }
