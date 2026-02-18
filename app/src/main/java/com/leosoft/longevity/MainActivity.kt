e61648afb16d2e7c3f458b6c01e9f037e2a6e579
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-18 23:31:16 +0300

    MainActivity için eksik TextButton importunu ekle

--- app/src/main/java/com/leosoft/longevity/MainActivity.kt
+++ app/src/main/java/com/leosoft/longevity/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
