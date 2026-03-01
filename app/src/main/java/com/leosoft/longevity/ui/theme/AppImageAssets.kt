b9eb5259e6ddfa515581dbc8ed192e3a194ee878
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-03-02 00:06:39 +0300

    Fix notification icon resource holder compile error

--- app/src/main/java/com/leosoft/longevity/ui/theme/AppImageAssets.kt
+++ app/src/main/java/com/leosoft/longevity/ui/theme/AppImageAssets.kt
@@ -1,5 +1,6 @@
 package com.leosoft.longevity.ui.theme
 
+import androidx.annotation.DrawableRes
 import com.leosoft.longevity.R
 
 object AppImageAssets {
@@ -7,5 +8,6 @@ object AppImageAssets {
      * Notification small icon. Replace `res/drawable/ic_notification.xml`
      * with your own Android Studio Image Asset to customize globally.
      */
-    const val notificationSmallIconRes: Int = R.drawable.ic_notification
+    @DrawableRes
+    val notificationSmallIconRes: Int = R.drawable.ic_notification
 }
